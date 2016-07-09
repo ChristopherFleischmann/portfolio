25af72e8d233b5cbf1c60acdfa0a230e2063b56c
Kevin Sung <ksung@browsermedia.com>
2016-07-09 12:57:46 -0400

    build.sh changes

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-gulp build && git status && git add -A && git commit -m "Basic Update" && git push origin master && git subtree push --prefix dist origin gh-pages
+echo "Building Project"
+gulp build
+echo "Status"
+git status
+echo "initiating adding and commiting files"
+git add -A
+git commit
+ech "Pushing Data"
+git push origin master
+git subtree push --prefix dist origin gh-pages
+echo "Process Complete"
