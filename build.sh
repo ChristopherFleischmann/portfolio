82707ae3ceff7eea6a9ed15266d810af76354a51
Kevin Sung <ksung@browsermedia.com>
2016-07-09 12:33:47 -0400

    Basic Update

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-gulp build && git status && git add -A && git commit -m "Basic Update" && git push origin master && git subtree push --prefix dist origin gh-pages
+gulp build
+git status 
+git add -A
+git commit -m "Basic Update"
+git push origin master
+git subtree push --prefix dist origin gh-pages
